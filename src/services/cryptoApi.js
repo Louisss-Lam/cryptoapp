6a18222d814bdaebc931ac79278d13cea80652b7
louis <louislam617@gmail.com>
2022-07-12 17:30:06 +0100

    homepage updated

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': '81f32d6c58mshed2a37e181da90cp1e8d90jsn97e5e3750466',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
+
